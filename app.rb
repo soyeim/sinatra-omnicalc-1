122482912615ec31e1f636f153811f6d7866ba97
soyeim <146144729+soyeim@users.noreply.github.com>
2023-10-23 07:02:07 +0000

    payment

--- app.rb
+++ app.rb
@@ -28,3 +28,19 @@ get ("/square_root/results") do
 
   erb(:sqrt_results)
 end
+
+get("/payment/new") do
+  erb(:new_payment_calc)
+end
+
+get("/payment/results") do
+  @apr = params.fetch("user_apr").to_f/100
+  @yr = params.fetch("user_years").to_i
+  @period = @yr * 12
+  @pv = params.fetch("user_pv").to_f
+  @numerator = @apr * @pv
+  @denom = 1 - (1+ @apr) ** -@period
+
+  @result = @numerator/@denom
+  erb(:payment_results)
+end
